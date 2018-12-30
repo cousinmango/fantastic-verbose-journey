c8602906fb7286bdff4ceb5acfd8b9003ed3ecf4
SLimma <30815731+slimlime@users.noreply.github.com>
2018-12-30 16:11:15 +1000

    :sparkles: Utilities

--- FantasticVerboseJourney/Extensions/Utilities/Protocols/DebugPerformanceOptimisable.swift
+++ FantasticVerboseJourney/Extensions/Utilities/Protocols/DebugPerformanceOptimisable.swift
@@ -5,27 +5,8 @@
 //  Copyright © 2018 CousinMango. All rights reserved.
 //
 
-import Foundation
-import struct CoreGraphics.CGGeometry.CGPoint
-import struct CoreGraphics.CGGeometry.CGFloat
-
-infix operator +-: AdditionPrecedence
-infix operator ☺️: AdditionPrecedence // lol
-
-protocol VectorCalculatable  {
-
-    static func +(left: Self, right: Self) -> Self
-    static func +-(left: Self, right: Self) -> Self
-    static func -(left: Self, right: Self) -> Self
-    //    static func *(left: Self, right: Self) -> Self
-    //    static func /(left: Self, right: Self) -> Self
-    static func *(point: Self, scalar: CGFloat) -> Self
-    static func /(point: Self, scalar: CGFloat) -> Self
-
-
-    func length() -> CGFloat
-
-    func normalized() -> CGPoint
-
+/// Convenience setup for debugging or performance toggles
+protocol DebugPerformanceOptimisable {
+    func setPerformanceOptimisation(enable toggle: Bool)
+    func setShowDebug(enable toggle: Bool)
 }
-
