d6f2280c6a41f1a2907cee9f9b2e11270e37ae3c
Xcode Server <30815731+slimlime@users.noreply.github.com>
2019-02-23 06:59:53 +1000

    Spawn positioning helper functions and adjustment factors fleshing out refining the protocol extensions.

--- FantasticVerboseJourney/Views/Scenes/GameScene/Managers/SpawnPositions.swift
+++ FantasticVerboseJourney/Views/Scenes/GameScene/Managers/SpawnPositions.swift
@@ -7,6 +7,7 @@
 //
 
 import struct SpriteKit.CGPoint
+import struct SpriteKit.CGFloat
 
 /// Proportional distance away from centre. 1.0 scales to the centre to the edge (from 0.5 to 0.0 and 0.5 to 1.0)
 struct SpawnPositionProportion {
@@ -32,26 +33,52 @@ struct SpawnPositionProportion {
 // Hopefully stored property in the Mob object... might run into trouble here. Mutation already!
 // starting with static functions to more easily move into functional programming later.
 protocol PositionAdjustable {
-    static func closerToMid(spawnPosition: CGPoint) -> CGPoint
+    // Adjustment factors for scaling
+    //    static let linearIncrement = 0.05
+    //    static let mid = 0.5 // assumption
+    static var centre: CGPoint { get }
     
-    static func furtherFromMid(spawnPosition: CGPoint) -> CGPoint
+    static func closerToCentre(spawnPosition: CGPoint) -> CGPoint
+    
+    /// Moves the coordinate a bit further from the centre by the predefined amount.
+    ///
+    /// - Parameter spawnPosition: Original spawn position. e.g. of the intended spawn Mob.
+    /// - Returns: New point that is further from the centre by the defined amount
+    static func furtherFromCentre(spawnPosition: CGPoint) -> CGPoint
 }
 extension SpawnPositionProportion: PositionAdjustable {
     // safe from modification.. but maybe encapsulate some of the other mob spawn properties to prevent undue non-standard modification?
     private static let linearIncrement = 0.05
+    private static let mid = 0.5 // assumption
+    internal static let centre = CGPoint(
+        x: mid,
+        y: mid
+    )
     // functional style closer() closer() chain to alter by fixed amounts? does the same thing as *0.8*0.8 orrrr return a computed variable that derives from the previously defined coordinate positioning system definitions e.g. northwest
     // linear increment? vs fraction? math-e-mat-ics. 0.8*0.8 to bring closer to centre. but 0.1*0.8...
     // linear increment simplest is best to start with. 0.1 + 0.05 and 0.8 - 0.05
     // standard probably only makes sense for silly duckman four corners attack initial spawn system... sooo don't think too hard.
     // static func closer()
-    static func closerToMid(spawnPosition: CGPoint) -> CGPoint {
+    static func closerToCentre(spawnPosition: CGPoint) -> CGPoint {
+        
+    }
+    
+
+    static func furtherFromCentre(spawnPosition: CGPoint) -> CGPoint {
         <#code#>
     }
     
-    static func furtherFromMid(spawnPosition: CGPoint) -> CGPoint {
-        <#code#>
+    // mishmash of types continues in soft dev app v2.1
+    // helper to perform calc on x and y of CGPoint
+    // probably could be an extension of the base type instead... step decrement increment by given amount. (positive or negative)
+    private static func increment(givenPoint: CGPoint, increment: CGFloat) -> CGPoint {
+        let incrementedPoint = CGPoint(
+            x: givenPoint.x + increment,
+            y: givenPoint.y + increment
+        )
+        
+        
     }
-    
 
 
 }
